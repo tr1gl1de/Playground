8238ebb11e20dc7e9eba7667bbfd4d629e161035
Maxim <pogatop@yandex.ru>
2022-09-13 21:42:54 +0200

    Update: change TestUtils class on static also add doc for CompletesIn method.

--- ExercisesTests/Utils/TestUtils.cs
+++ ExercisesTests/Utils/TestUtils.cs
@@ -1,7 +1,14 @@
 namespace ExercisesTests.Utils;
 
-public class TestUtils
+public static class TestUtils
 {
+    /// <summary>
+    /// Method for limiting the execution of method by time.
+    /// </summary>
+    /// <param name="timeoutIsSeconds">waiting time of seconds</param>
+    /// <param name="action">Action for waiting method</param>
+    /// <exception cref="Exception">exception in action method</exception>
+    /// <exception cref="TimeoutException">thrown when completed timeout</exception>
     public static void CompletesIn(int timeoutIsSeconds, Action action)
     {
         var task = Task.Run(action);
