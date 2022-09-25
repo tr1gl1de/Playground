9ee94dd0551eb76468b996d68e7c81521622f0da
Maxim <pogatop@yandex.ru>
2022-09-25 17:07:39 +0200

    Add endpoints for Product controller

--- MediatrExample/Controllers/ProductsController.cs
+++ MediatrExample/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+using MediatrExample.Commands;
+using MediatrExample.Queries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +19,27 @@ namespace MediatrExample.Controllers
         public ProductsController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts()
+        {
+            var products = await _mediator.Send(new GetProductsQuery());
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct([FromBody] Product product)
+        {
+            var createdProduct = await _mediator.Send(new AddProductCommand(product));
+            return CreatedAtRoute("GetProductByIdAsync", new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpGet("{id:long}", Name = "GetProductByIdAsync")]
+        public async Task<ActionResult> GetProductById(long id)
+        {
+            var product = await _mediator.Send(new GetProductByIdQuery(id));
+            return Ok(product);
         }
     }
 }
